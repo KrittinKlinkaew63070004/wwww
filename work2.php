55729b152f58321226466a89f69706b59290ef85
KrittinKlinkaew63070004 <73655235+KrittinKlinkaew63070004@users.noreply.github.com>
2020-10-30 10:53:53 +0700

    Update work2.php

--- work2.php
+++ work2.php
@@ -1,4 +1,19 @@
-<?php
-echo "<h2> Krittin Klinkaew 63070004 (Neua)</h2>";
-echo "<p>ขำ ขำมาก ขำ</p>";
-?>
+<html>
+    <head>
+        <title> Hello World</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragraph</p>
+        <img src = "cat.jpg">
+        <br>
+        <a href="htttp://www.it.kmitl.ac.th">This is a link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    </body>
+</html>
