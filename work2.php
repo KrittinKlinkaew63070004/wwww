c787e40d55f9171e20dc12dcc33e47b608f98b05
KrittinKlinkaew63070004 <73655235+KrittinKlinkaew63070004@users.noreply.github.com>
2020-10-30 10:44:33 +0700

    Create work2.php

---
+++ work2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Krittin Klinkaew(Neua) 63070004</title>
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+       
+        <img src = "cat.jpg">
+        <br>
+        <a href="htttp://www.it.kmitl.ac.th">This is a link</a>
+        <p>ขำ ขำมาก ขำ</p>
+        
+    </body>
+</html>
