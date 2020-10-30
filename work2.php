da96ac055fad362e0fdbfa0bf8bdda9547cd0a74
KrittinKlinkaew63070004 <73655235+KrittinKlinkaew63070004@users.noreply.github.com>
2020-10-30 10:57:40 +0700

    Update work2.php

--- work2.php
+++ work2.php
@@ -1,19 +1,17 @@
 <html>
     <head>
-        <title> Hello World</title>
+        <title> Krittin Klinkaew (Neua) 63070004</title>
     </head>
     <body>
-        <h1>My First Heading</h1>
-        <p>My First Paragraph</p>
         <img src = "cat.jpg">
+        <img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.newshub.co.nz%2Fhome%2Flifestyle%2F2019%2F08%2Fthe-top-five-cat-memes-of-all-time-rated.html&psig=AOvVaw0LCXZQowIUZSmDq8pbrsSc&ust=1604116476741000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDgl9612-wCFQAAAAAdAAAAABAO">
         <br>
         <a href="htttp://www.it.kmitl.ac.th">This is a link</a>
-
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
-        </ul>
+         <a href="htttp://www.youtube.com">This is a link</a>
+        <p>A:วันก่อนครับ</p>
+        <p>B:ทำไมครับ</p>
+        <p>A:วันนี้หลวม</p>
+        <p>B:อ่อเค</p>
+       
     </body>
 </html>
